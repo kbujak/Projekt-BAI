91e03346c474ec1f219a090b625dcc1034e45a94
mboryczko <m.boryczko@telemedi.co>
2019-05-05 20:59:36 +0200

    initial commit with basic google sign-in

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'AuthService.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'FlutterBase',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Project BAI'),
+              backgroundColor: Colors.amber,
+            ),
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  LoginButton(), // <-- Built with StreamBuilder
+                  UserProfile() // <-- Built with StatefulWidget
+                ],
+              ),
+            )
+        )
+    );
+  }
+}
+
+class UserProfile extends StatefulWidget {
+  @override
+  UserProfileState createState() => UserProfileState();
+}
+
+class UserProfileState extends State<UserProfile> {
+  Map<String, dynamic> _profile;
+  bool _loading = false;
+
+  @override
+  initState() {
+    super.initState();
+
+    // Subscriptions are created here
+    authService.profile.listen((state) => setState(() => _profile = state));
+
+    authService.loading.listen((state) => setState(() => _loading = state));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
+      Text(_loading.toString())
+    ]);
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: authService.user,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return MaterialButton(
+              onPressed: () => authService.signOut(),
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text('Signout'),
+            );
+          } else {
+            return MaterialButton(
+              onPressed: () => authService.googleSignIn(),
+              color: Colors.white,
+              textColor: Colors.black,
+              child: Text('Login with Google'),
+            );
+          }
+        });
+  }
+}
