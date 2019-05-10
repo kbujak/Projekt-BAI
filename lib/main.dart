e6f8947aa19331b4207592805dd0156488c2c014
mboryczko <m.boryczko@telemedi.co>
2019-05-10 12:43:34 +0200

    [1] home page user button impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,37 @@
 import 'package:flutter/material.dart';
-import 'AuthService.dart';
+import 'signin.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HomeRoute());
 
-class MyApp extends StatelessWidget {
+class HomeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'FlutterBase',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Project BAI'),
-              backgroundColor: Colors.amber,
-            ),
-            body: Center(
-              child: Column(
-                children: <Widget>[
-                  LoginButton(), // <-- Built with StreamBuilder
-                  UserProfile() // <-- Built with StatefulWidget
-                ],
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Project BAI'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Welcome to MAKK chat'),
+              RaisedButton(
+                child: Text('User'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+              ), // <-- Built with StreamBuilder
+              RaisedButton(
+                child: Text('Guest'),
+                onPressed: () {
+                },
               ),
-            )
-        )
-    );
-  }
-}
-
-class UserProfile extends StatefulWidget {
-  @override
-  UserProfileState createState() => UserProfileState();
-}
-
-class UserProfileState extends State<UserProfile> {
-  Map<String, dynamic> _profile;
-  bool _loading = false;
-
-  @override
-  initState() {
-    super.initState();
-
-    // Subscriptions are created here
-    authService.profile.listen((state) => setState(() => _profile = state));
-
-    authService.loading.listen((state) => setState(() => _loading = state));
+            ],
+          ),
+        ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(padding: EdgeInsets.all(20), child: Text(_profile.toString())),
-      Text(_loading.toString())
-    ]);
-  }
-}
-
-class LoginButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: authService.user,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return MaterialButton(
-              onPressed: () => authService.signOut(),
-              color: Colors.red,
-              textColor: Colors.white,
-              child: Text('Signout'),
-            );
-          } else {
-            return MaterialButton(
-              onPressed: () => authService.googleSignIn(),
-              color: Colors.white,
-              textColor: Colors.black,
-              child: Text('Login with Google'),
-            );
-          }
-        });
-  }
 }
