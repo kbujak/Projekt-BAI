374c91b1cbe950a0032aa3aff11021359d441a13
mboryczko <m.boryczko@telemedi.co>
2019-05-11 17:59:04 +0200

    fix for home page error

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,41 @@
 import 'package:flutter/material.dart';
 import 'signin.dart';
 
-void main() => runApp(HomeRoute());
+void main() {
+  runApp(MaterialApp(
+    title: 'Project BAI',
+    home: HomeRoute(),
+  ));
+}
 
 class HomeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Project BAI'),
-          backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Welcome to MAKK chat'),
+            RaisedButton(
+              child: Text('User'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyApp()),
+                );
+              },
+            ),
+            RaisedButton(
+              child: Text('Guest'),
+              onPressed: () {
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Text('Welcome to MAKK chat'),
-              RaisedButton(
-                child: Text('User'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyApp()),
-                  );
-                },
-              ), // <-- Built with StreamBuilder
-              RaisedButton(
-                child: Text('Guest'),
-                onPressed: () {
-                },
-              ),
-            ],
-          ),
-        ));
-  }
+      ));
+}
 
 }
